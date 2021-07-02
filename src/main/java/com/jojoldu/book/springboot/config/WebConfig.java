c4a9cef60b38948ff577fb3c6e8817976cf879fc
epson220 <62466439+epson220@users.noreply.github.com>
2021-07-02 18:02:17 +0900

    리엑트랑 소셜 로그인테스트21

--- src/main/java/com/jojoldu/book/springboot/config/WebConfig.java
+++ src/main/java/com/jojoldu/book/springboot/config/WebConfig.java
@@ -18,6 +18,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("**")
+                .allowedMethods("*")
+                .allowCredentials(true)
                 .allowedOrigins("*");
     }
 
